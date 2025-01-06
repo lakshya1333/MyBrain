7d0b61880f5c1a976db7122c86e83b2ef56f6f38
Lakshya Jain <148108892+lakshya1333@users.noreply.github.com>
2025-01-06 10:28:11 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { ContentModel, LinkModel, UserModel } from "./db"
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 import jwt from "jsonwebtoken"
 import z from "zod"
 import bcrypt from "bcrypt"
